80eb7ab07b642ab108c8b264c1951638e1c55e06
Sergiu <sergiu.anghelenici@corlab.tech>
2023-06-30 18:02:19 +0300

    install next-pwa

--- public/sw.js
+++ public/sw.js
@@ -67,7 +67,7 @@ if (!self.define) {
     });
   };
 }
-define(['./workbox-74295486'], (function (workbox) { 'use strict';
+define(['./workbox-e34f44db'], (function (workbox) { 'use strict';
 
   importScripts();
   self.skipWaiting();
