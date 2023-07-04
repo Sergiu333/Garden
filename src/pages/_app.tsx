59fe656f737269f702da1b450bf06e916fc921b7
Sergiu <sergiu.anghelenici@corlab.tech>
2023-07-04 15:05:09 +0300

    google analytics #2

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -17,6 +18,20 @@ export default function App({ Component, pageProps }: AppProps) {
         <link rel="icon" href="/foisoare.ico" />
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/new.png" />
+          <Script
+              strategy="lazyOnload"
+              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`}
+          />
+          <Script id="ga-script" strategy="lazyOnload">
+              {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.GOOGLE_ANALYTICS}', {
+                  page_path: window.location.pathname,
+                });
+                 `}
+          </Script>
       </Head>
       <Component {...pageProps} />
     </>
